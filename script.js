5f79fbb69487fe1baede6e42b222d5c04b21045c
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-10-26 13:46:25 +0600

    Sidebar get data

--- script.js
+++ script.js
@@ -1,28 +1,92 @@
 // For all button
-
 const allBtn = document.getElementsByClassName("add-to-order")
 
 for (const btn of allBtn) {
   btn.addEventListener("click", function (event) {
     const name = event.target.parentNode.childNodes[1].innerText
-    const image = event.target.parentNode.parentNode.childNodes[3]
+    const imageSrc = event.target.parentNode.parentNode.childNodes[3].src
     const price = event.target.parentNode.childNodes[3].childNodes[0].innerText
-    // console.log(name, price)
-    const selectedContainer = document.getElementById("cart-sidebar")
 
-    const div = document.createElement("div")
-    div.classList.add("cart-item-info")
+    // Find the cart sidebar container
+    const cartSidebar = document.getElementById("cart-sidebar")
 
-    const p1 = document.createElement("p")
-    const p2 = document.createElement("p")
+    // Create cart item container
+    const cartItemDiv = document.createElement("div")
+    cartItemDiv.classList.add("cart-item", "border", "rounded")
 
-    p1.innerText = name
-    p2.innerText = price
+    // Create and append image element
+    const imgElement = document.createElement("img")
+    imgElement.src = imageSrc
+    imgElement.alt = name
+    imgElement.classList.add("cart-item-image")
+    cartItemDiv.appendChild(imgElement)
 
-    div.appendChild(p1)
-    div.appendChild(p2)
+    // Create cart item info container
+    const infoDiv = document.createElement("div")
+    infoDiv.classList.add("cart-item-info")
 
-    selectedContainer.appendChild(div)
+    // Create and append item name
+    const itemName = document.createElement("h3")
+    itemName.innerText = name
+    infoDiv.appendChild(itemName)
+
+    // Create and append price paragraph
+    const pricePara = document.createElement("p")
+    pricePara.innerText = price
+    infoDiv.appendChild(pricePara)
+
+    // Create quantity control div
+    const quantityControlDiv = document.createElement("div")
+    quantityControlDiv.classList.add("quantity-control")
+
+    // Create "-" button
+    const minusButton = document.createElement("button")
+    minusButton.classList.add("quantity-btn")
+    minusButton.innerText = "-"
+    quantityControlDiv.appendChild(minusButton)
+
+    // Create quantity input
+    const quantityInput = document.createElement("input")
+    quantityInput.classList.add("w-[100px]", "text-black")
+    quantityInput.type = "number"
+    quantityInput.value = "1"
+    quantityInput.min = "1"
+    quantityControlDiv.appendChild(quantityInput)
+
+    // Create "+" button
+    const plusButton = document.createElement("button")
+    plusButton.classList.add("quantity-btn")
+    plusButton.innerText = "+"
+    quantityControlDiv.appendChild(plusButton)
+
+    // Append quantity control to info div
+    infoDiv.appendChild(quantityControlDiv)
+
+    // Append infoDiv to the main cart item div
+    cartItemDiv.appendChild(infoDiv)
+
+    // Append the complete cart item div to the cart sidebar
+    cartSidebar.appendChild(cartItemDiv)
+
+    // Update or add the cart total and place order button if not already present
+    if (!document.querySelector(".cart-total")) {
+      const cartTotalDiv = document.createElement("div")
+      cartTotalDiv.classList.add("cart-total")
+
+      const totalPara = document.createElement("p")
+      totalPara.innerHTML = "Total: <span>$20.95</span>" // Placeholder value
+      cartTotalDiv.appendChild(totalPara)
+
+      cartSidebar.appendChild(cartTotalDiv)
+    }
+
+    if (!document.querySelector(".place-order")) {
+      const placeOrderButton = document.createElement("button")
+      placeOrderButton.classList.add("place-order")
+      placeOrderButton.innerText = "Place Order"
+
+      cartSidebar.appendChild(placeOrderButton)
+    }
   })
 }
 
